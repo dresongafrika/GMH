4cc86f467d298c8089798f62635b2a7077da41b5
Damilare ADEMESO <dresongafrika@gmail.com>
2017-09-08 09:03:03 -0700

    payment gateway url resolved

--- membership.php
+++ membership.php
@@ -102,18 +102,19 @@
                     </form>';
                 ?>
                     <div id="pay">
-                        <form method="POST" action="test_pay.php?name=<?php echo $uName;?>">
+                        <form method="POST" action="https://voguepay.com/pay/">
                             Please proceed to pay.
-                            <input type="hidden" name="v_merchant_id" value="3828-0054426" />
+                            <input type="hidden" name="v_merchant_id" value="demo" />
                             <input type="hidden" name="memo" value="payment for promotional song upload" />
-                            <input type="hidden" name="success_url" value="<?php echo $_SERVER['SERVER_NAME']; ?>/member_create.php?pay=yes&username=<?php echo $uName; ?>" />
-                            <input type="hidden" name="fail_url" value="<?php echo $_SERVER['SERVER_NAME']; ?>/member_create.php?pay=no" />
+                            <input type="hidden" name="success_url" value="http://www.gospelmusichotspot.com/member_create.php?pay=yes&username=<?php echo $uName; ?>" />
+                            <input type="hidden" name="fail_url" value="http://www.gospelmusichotspot.com/member_create.php?pay=no" />
+							<input type="hidden" name="notify_url" value="http://www.gospelmusichotspot.com/member_create.php?" />
                             <input type="hidden" name="cur" value="NGN" />
                             <input type="hidden" name="item_1" value="upload" />
                             <input type="hidden" name="developer_code" value="599a05bc1e8d3" />
                             <input type="hidden" name="total" value="10000" />
                             <input type="hidden" name="description_1" value="" /><br />
-                            <input type="submit" value="pay" alt="PAY WITH YOUR CREDIT/DEBIT CARD" />
+                            <input type="image" src="https://voguepay.com/images/buttons/make_payment_blue.png" alt="PAY WITH YOUR CREDIT/DEBIT CARD" />
                         </form>
                         <i class="fa fa-cc-discover" ></i><i class="fa fa-cc-visa"></i><i class="fa fa-cc-mastercard"></i><i class="fa fa-cc-paypal"></i></br>
                     </div>
